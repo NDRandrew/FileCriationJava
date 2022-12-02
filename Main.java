231e5e0fa080a9dd5db3d0ccc13b4623a7ca642e
Andrew <50187249+NDRandrew@users.noreply.github.com>
2022-12-02 12:10:32 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
 class Main {
   public static void main(String[] args) {
     Arquivos File = new Arquivos();
-    String path = "exercicios/teste1.txt";
-    String texto = "\n Aula de criar arquivo e escrever em existente.";
-    File.createFile(path);
-    File.writeFile(path,texto);
-    File.readFile(path);
-    File.deleteFile(path);
-    File.createDir("exercicios");
-    File.listDir("exercicios");
+    String path = "exercicios/teste1.txt"; //Where file will be created
+    String texto = "\n Aula de criar arquivo e escrever em existente."; //Text that will be added inside the existing or created file
+    File.createFile(path); //Create a file in Path
+    File.writeFile(path,texto); //Write method
+    File.readFile(path); //Read Method
+    File.deleteFile(path); //Delete file method
+    File.createDir("exercicios"); //Create directory
+    File.listDir("exercicios"); // Add directory into an array
   }
-}
+} 
+// ALL METHODS ARE BEING USED FOR THE SAKE OF TESTING.
+// You could remove delete for the file only be created for example
+// Software is still in it starting point, can be refined
